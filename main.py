73e5002399fcac503fbf43f0490965b282b29775
Francois <fnlombard@gmail.com>
2024-02-04 11:45:21 +0100

    Saving video works.

--- main.py
+++ main.py
@@ -1,17 +1,52 @@
+from dataclasses import dataclass
 import os
 from pathlib import Path
+import subprocess
 import cv2
 import numpy as np
 import argparse
+import tempfile
 
 Matt = np.ndarray[np.uint8]
 Video = np.ndarray
 
 
+@dataclass
+class VideoMetaData:
+    fps: int
+    width: int
+    height: int
+    path: Path
+
+
+@dataclass
+class Video:
+    data: np.ndarray
+    metadata: VideoMetaData
+
+
 def load_video(input_file: Path) -> Video:
-    cap = cv2.VideoCapture(input_file)
+    cap = cv2.VideoCapture(str(input_file))
     file_type = input_file.suffix
     file_name = input_file.stem
+
+    ret, frame = cap.read()
+    ret_val = Video(
+        data=[],
+        metadata=VideoMetaData(
+            fps=cap.get(cv2.CAP_PROP_FPS),
+            width=int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+            height=int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
+            path=input_file,
+        ),
+    )
+
+    while ret:
+        ret_val.data.append(frame)
+        ret, frame = cap.read()
+
+    cap.release()
+    return ret_val
 
     # grab one frame
     scale = 0.5
@@ -55,6 +90,48 @@ def load_video(input_file: Path) -> Video:
         # get the color mask
         margin = 50
         mask = cv2.inRange(s, big_color - margin, big_color + margin)
+
+
+def save_video(video: Video) -> None:
+    output = video.metadata.path.parent / (video.metadata.path.stem + "_final.webm")
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_dir_path = Path(temp_dir)
+        for id, frame in enumerate(video.data):
+            image_path = temp_dir / Path(f"frame_{id:04d}.png")
+            cv2.imwrite(str(image_path), frame)
+
+        command = (
+            "ffmpeg",
+            "-y",
+            "-framerate",
+            str(video.metadata.fps),
+            "-i",
+            str(
+                temp_dir_path / "frame_%04d.png"
+            ),  # Specify the full path for the input frames
+            "-c:v",
+            "libvpx-vp9",
+            "-b:v",
+            "1000k",
+            "-minrate",
+            "500k",
+            "-maxrate",
+            "1500k",
+            "-crf",
+            "20",
+            "-pix_fmt",
+            "yuva420p",
+            "-auto-alt-ref",
+            "0",
+            "-metadata:s:v:0",
+            'alpha_mode="1"',
+            "-c:a",
+            "libvorbis",
+            "-an",  # No audio
+            str(output),
+        )
+        subprocess.run(command, check=True, shell=False)
 
 
 def remove_screen(image: Matt) -> Matt:
@@ -114,11 +191,8 @@ def main() -> None:
     if not (input_file := Path(args.input_file)).exists() and not input_file.is_file():
         raise RuntimeError(f"{input_file} does not exist or is not a file.")
 
-    output_directory = input_file.parent
-    file_type = input_file.suffix
-    file_name = input_file.stem
-
     input_video = load_video(input_file)
+    save_video(input_video)
 
 
 if __name__ == "__main__":
